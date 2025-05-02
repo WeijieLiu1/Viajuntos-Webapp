e33cc62b395eaa5ee5d42e2d2a7d4707faf967ab
WeijieLiu1 <weijie.liu@estudiantat.upc.edu>
2025-05-02 15:59:47 +0800

    update path

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-//   baseURL: 'https://socialout-production.herokuapp.com',
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://viajuntos-backend-284495715917.europe-southwest1.run.app/',
+  // baseURL: 'http://127.0.0.1:5000',
   headers: {
     'Content-type': 'application/json',
     'Accept': 'application/json',
